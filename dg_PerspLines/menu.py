1f78eea416d68c7598b634e24e41bd6c99e28446
BullS10 <45762560+BullS10@users.noreply.github.com>
2022-03-14 22:51:25 +0100

    Add files via upload

---
+++ dg_PerspLines/menu.py
@@ -0,0 +1,20 @@
+import nuke
+
+def dg_PerspLines_Horizon():
+	nodes=nuke.selectedNodes()
+	if not len(nodes)==2:
+		nuke.message('Illegal amount of selected nodes.\nPlease select exactly two dg_PerspLines nodes')
+		return
+	for n in nodes:
+		if not n.Class()=='dg_PerspLines':
+			nuke.message('One of selected nodes is not dg_PerspLines')
+			return
+	i=1
+	if nodes[0].input(0)==nodes[1]:
+		i=0
+	
+	dg_PerspLines_selectOnly(nodes[i])
+	
+	n=nuke.createNode('dg_Horizon')
+	n['vp1'].setExpression(nodes[0].name()+'.PT')
+	n['vp2'].setExpression(nodes[1].name()+'.PT')
